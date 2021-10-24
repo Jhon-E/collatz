50db98cdfe7bb1047ec08e393a79fa38d2b70d39
Jhone <jhoneospino@gmail.com>
2021-10-23 19:06:56 -0500

    version 1.2 collatz

--- indexColl.js
+++ indexColl.js
@@ -1,43 +1,56 @@
 let btn = document.querySelector("#submit");
 let num = document.querySelector("#num");
+let boxR = document.querySelector('.result');
 google.charts.load('current', {'packages':['bar']});
 
-let pasos = 0;
-
-
+// FUNCION MAIN CONJETURA DE COLLATZ
 const eventoBoton = btn.addEventListener("click", function () {
-
+    let pasos = 0;
     let c = num.value;
     let paso = [];
     let cantidad = [];
+
+  //VALIDACIÓN DEL INPUT
   if (c != "" || c > 0 && c == Number){ 
+
+    //ALGORITMO DE LA CONJETURA
     while (c != 1){
+          //SI ES PAR DIVIDIRLO ENTRE 2
           if (c % 2 == 0){
             c /= 2;
-          } else {
+          } // SINO MULTIPLICARLO POR 3 Y SUMARLE 1 
+          else {
             c = (3 * c) + 1; 
           }
+          //CONTADOR DE LOS PASOS E INSTANCIA EL ARRAY _PASO_
             cantidad.push(c);
             pasos++;
             paso.push(pasos);
     }
  
-        console.log(`%c${pasos}`,`color: green; font-size:20px;`);
-        console.log(paso)
-        console.log(cantidad)
-        pasos = pasos - pasos;
+/* console.log(`%c${pasos}`,`color: green; font-size:20px;`);
+console.log(paso)
+console.log(cantidad) */
+//ESTO SIRVE PARA QUE NO SE ACUMULEN LOS PASOS SI EL USUARIO DIGITA VARIOS INPUT'S
+pasos = pasos - pasos;
 
-        /* DIBUJA EL GRAFICO */
+let re = `<div class = "msg">
+          <h2><b>Se redujo (<span>${num.value}</span>) en <span>${paso[paso.length-1]}</span> pasos.</b></h2>
+          </div>`
+          console.log(c)
+boxR.innerHTML = re;
 
+
+/* DIBUJA EL GRAFICO */
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  // Some raw data (not necessarily accurate) 
+  // DATA DEL GRAFICO
   var data = google.visualization.arrayToDataTable([
     ['Pasos','Resultado', {role: 'style'}],
   ...Array(paso,...cantidad).fill(0).map((value,index)=>[`paso ${[index + 1]}`,cantidad[index], 'color: black'])
 ]);
-
+  //OPCIONES DEL GRAFICO
   var options = {
     chart: {
       title: 'Gráfico interactivo'
